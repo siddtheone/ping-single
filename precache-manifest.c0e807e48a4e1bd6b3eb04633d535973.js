0bc0539f1242ea323bfb5f815df63a9ee794e66e
siddtheone <siddharthl@cybage.com>
2020-05-28 15:42:54 +0530

    Updates

---
+++ precache-manifest.c0e807e48a4e1bd6b3eb04633d535973.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9c93a93c1994833ec5fb698ba5a80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a87dab86a79f23a5e8f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "269aa7c7b2463a225e06",
+    "url": "/static/js/2.828a3411.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.828a3411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a87dab86a79f23a5e8f",
+    "url": "/static/js/main.49c3ea8b.chunk.js"
+  },
+  {
+    "revision": "1e95c12c3bfbcf947c1b",
+    "url": "/static/js/runtime-main.7ebcb835.js"
+  },
+  {
+    "revision": "92dd5c8fe5e8dde945a6f01abca7e7cc",
+    "url": "/static/media/illustration-dashboard.92dd5c8f.png"
+  },
+  {
+    "revision": "e392d534418b8f8c25ecb73731147704",
+    "url": "/static/media/logo.e392d534.svg"
+  }
+]);
